36b905e2211f648a45bb697b3c11867e8147b860
hringriin <hringriin@gmail.com>
2018-01-13 18:55:53 +0100

    [ install all ] global config file added

--- ssh/create_config.bash
+++ ssh/create_config.bash
@@ -1,8 +1,33 @@
 #!/bin/bash
-# create_config for -- <<PROGNAME>>
+# create_config for -- SSH
+
+prgname="SSH"
+
+echo -e "\e[1;36mInstalling ... ${prgname} ... configuration files ...\e[0m"
+sleep 1
+
+source INSTALL_ALL/config.bash
 
 main()
 {
+    # ssh
+    if [[ ! ( -d ~/.ssh ) ]] ; then
+        mkdir --mode=700 ~/.ssh
+    fi
+    cp -ifv ${repoPath}/ssh/config ~/.ssh/config
+
+    echo -e "Please unencrypt the ssh config with vim."
+    read -p "Do it now? [Y/n]: " decrypt
+
+    if [[ ${decrypt} == "y" || ${decrypt} == "Y" || ${decrypt} == "" ]] ; then
+        vim ~/.ssh/config
+    elif
+        [[ ${decrypt} == "n" || ${decrypt} == "N" ]] ; then
+        echo -e "Will not touch ssh config."
+        echo -e "Please decrypt it by yourself before using ssh!"
+    else
+        echo -e "Unrecognized entry."
+    fi
 }
 
 main
